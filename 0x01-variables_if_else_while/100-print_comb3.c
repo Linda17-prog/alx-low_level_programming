48df467cb49a6bc7db175a39d6d18a155c1a500b
Linda17-prog <mwongelilinda@gmail.com>
2022-03-11 14:48:22 -0800

    100-print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,38 +5,32 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int i;
-	int j;
-	
-	i = 48;
-	j = 49;
-	
-	while  ((i < 57) && (j < 58))
+	int i = 48;
+	int j = 48;
+
+	while (j <58)
 	{
-		putchar(i);
-		putchar(j);
-		if ((i == 56) && (j == 57))
+		i = 48;
+		while (i < 58)
 		{
-			putchar('\n');
+			if (j != i && j < i)
+			{
+				putchar(j);
+				putchar(i);
+				if (i == 57 && j == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
 			i++;
-			j++;
 		}
-		else
-		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				j++;
-			}
-			else
-			{
-				i++;
-				j = 1 + i;
-			}
-		}
+		j++;
 	}
+	putchar('\n');
 	return (0);
 }
