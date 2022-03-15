108c1a0d304f9ba80019935f977c966e8972727a
Linda17-prog <mwongelilinda@gmail.com>
2022-03-15 11:35:35 -0700

    103-fibonnaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	long int i;
-	long int first = 1;
+	long int first = 0;
 	long int second = 1;
-	long int sum = 2 ;
+	long int sum = 0;
 	long int total;
 
 	for (i = 1; i < 4000000; i++)
@@ -25,5 +25,5 @@ int main(void)
 
 	printf("%1ld\n", total);
 
-	return(0);
+	return (0);
 }
