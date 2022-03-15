795bf9faddfaf12302cf9c198d043607ee5f9f2d
Linda17-prog <mwongelilinda@gmail.com>
2022-03-15 10:11:19 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integres
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of the two integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
