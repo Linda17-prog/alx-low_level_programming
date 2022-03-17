ae03fb39aac8f53b5e50db5d98f3aa0b253e4788
Linda17-prog <mwongelilinda@gmail.com>
2022-03-17 16:05:23 -0700

    Print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,20 +11,19 @@ void print_square(int size)
 	int i;
 	int j;
 
-	if (size > 0)
+	if (size <= 0)
 	{
-		for (i = 0; i < size; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				_putchar('#');
 			}
-
-			if (i == size - 1)
-				continue;
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
-			
