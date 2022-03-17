bf03d80a0dcf3feee2e45c9932a10b379d5bb1cd
Linda17-prog <99673742+Linda17-prog@users.noreply.github.com>
2022-03-18 01:49:11 +0300

    number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,20 +2,29 @@
 /**
  * print_number - Prints an integer
  * @n: Integer to be printed
- *
- * Return: void
+ * Return: No return
  */
+
 void print_number(int n)
 {
-	unsigned int num = n;
 
-	if (n < 0)
+	unsigned int n1 = 0;
+
+	if  (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		num = -num;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
 
-	_putchar((num % 10) + '0');
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
