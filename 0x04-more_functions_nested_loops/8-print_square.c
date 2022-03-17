91c7960bad2baf541143f49939aa4e4038fc13c7
Linda17-prog <mwongelilinda@gmail.com>
2022-03-17 16:27:08 -0700

    8-square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,9 +16,9 @@ void print_square(int size)
 	}
 	else
 	{
-		for (row = 1; row < size; row++)
+		for (row = 1; row <= size; row++)
 		{
-			for (column = 1; column < size; column++)
+			for (column = 1; column <= size; column++)
 			{
 				_putchar('#');
 			}
