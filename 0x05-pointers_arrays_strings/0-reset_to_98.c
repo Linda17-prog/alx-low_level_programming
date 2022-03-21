874603af97782c005159839a35862bf00da1f06c
Linda17-prog <mwongelilinda@gmail.com>
2022-03-21 12:24:54 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Reset the variable sent to the function to 98
+ * @n: Pointer to the integer sent to the function
+ *
+ * Return: Void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
