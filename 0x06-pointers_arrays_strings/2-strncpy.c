eecc97af91affcf2302af8c02d53917f191d0c36
Linda17-prog <mwongelilinda@gmail.com>
2022-03-24 06:41:23 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination string
+ * @src: String to be copied
+ *
+ * Return: Void
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0' && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
+}
