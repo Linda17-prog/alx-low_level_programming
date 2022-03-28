afd38ba16ebe8dbdb5f210c2859769ea8ebae3e4
Linda17-prog <99673742+Linda17-prog@users.noreply.github.com>
2022-03-28 22:59:51 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,15 +15,11 @@ unsigned int _strspn(char *s, char *accept)
 	while (accept[a_len] != '\0')
 		a_len++;
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		for (j = 0; j < a_len; j++)
-		{
 			if (s[i] == accept[j])
 				len++, j = a_len;
 			else
 				if (j == a_len - 1)
 					goto exit;
-		}
-	}
 exit: return (len);
 }
