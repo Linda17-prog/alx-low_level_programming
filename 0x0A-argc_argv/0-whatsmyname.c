d012cf4ebffacb255abb98c022a00ac41f4ad220
Linda17-prog <mwongelilinda@gmail.com>
2022-04-01 12:11:53 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of arguments passed
+ * @argv: Pointer to arrat of arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[0])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
