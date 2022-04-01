60826a0f1897c72e20a132eba4d4c1713ce2b518
Linda17-prog <99673742+Linda17-prog@users.noreply.github.com>
2022-04-01 23:11:29 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,4 +11,30 @@
 
 int main(int argc, char *argv[])
 {
+	int i;
+	int j = 0;
+	unsigned int add = 0;
 
+	for (i = 1; i < argc; i++)
+	{
+		while (*(*(argv + i) + j) != '\0')
+		{
+			if ((*(*(argv + i) + j) >= '0' && *(*(argv + i) + j) <= '9')
+			|| *(*(argv + i) + j) == '-')
+			{
+				j++;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		j = 0;
+
+		if (atoi(argv[i]) > 0)
+			add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
