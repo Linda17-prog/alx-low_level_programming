e87dfbaf0fe67317a6d23211b89b6386a2372683
Linda17-prog <mwongelilinda@gmail.com>
2022-04-01 13:08:57 -0700

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the addition of arguments
+ * @argc: Number of arguments passed
+ * @argv: Pointer to the array of arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
